84ccae8d5e512c48281d3a15044250fac8b575ca
tpluscode <tpluscode@users.noreply.github.com>
2019-12-19 16:47:35 +0100

    chore: fix test nesting

--- src/__tests__/decorator-resource.spec.ts
+++ src/__tests__/decorator-resource.spec.ts
@@ -84,11 +84,10 @@ describe('decorator', () => {
       // then
       expect(instance.name).toEqual('John Doe')
     })
-  })
 
-  it('returns string value of literal', async () => {
+    it('returns boolean when type is set', async () => {
     // given
-    const dataset = await parse(`
+      const dataset = await parse(`
         @prefix ex: <${prefixes.ex}> .
         @prefix schema: <${prefixes.schema}> .
         @prefix xsd: <${prefixes.xsd}> .
@@ -96,22 +95,23 @@ describe('decorator', () => {
         ex:res ex:isMarried true .
         ex:res ex:isSingle "false"^^xsd:boolean .
       `)
-    class Resource extends RdfResource {
-      @literal({ path: ex.isMarried, type: Boolean })
-      married?: boolean
+      class Resource extends RdfResource {
+        @literal({ path: ex.isMarried, type: Boolean })
+        married?: boolean
 
-      @literal({ path: ex.isSingle, type: Boolean })
-      single?: boolean
-    }
+        @literal({ path: ex.isSingle, type: Boolean })
+        single?: boolean
+      }
 
-    // when
-    const instance = new Resource({
-      dataset,
-      term: ex.res,
+      // when
+      const instance = new Resource({
+        dataset,
+        term: ex.res,
+      })
+
+      // then
+      expect(instance.single).toStrictEqual(false)
+      expect(instance.married).toStrictEqual(true)
     })
-
-    // then
-    expect(instance.single).toStrictEqual(false)
-    expect(instance.married).toStrictEqual(true)
   })
 })
