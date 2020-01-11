5fa8a596120de9a83d06a435308701c25aa782d7
tpluscode <tpluscode@users.noreply.github.com>
2020-01-11 14:33:44 +0100

    fix: proxy returns array for single object

--- src/__tests__/proxy.spec.ts
+++ src/__tests__/proxy.spec.ts
@@ -1,20 +1,25 @@
 import cf, { SingleContextClownface } from 'clownface'
 import $rdf from 'rdf-ext'
+import namespace from '@rdfjs/namespace'
 import { createProxy } from '../lib/proxy'
 import RdfResourceImpl from '../lib/RdfResource'
-import { DatasetCore, NamedNode } from 'rdf-js'
+import { DatasetCore, Literal, NamedNode } from 'rdf-js'
 import { property } from '..'
+
+const ex = namespace('http://example.com/')
 
 describe('proxy', () => {
   let node: SingleContextClownface<DatasetCore, NamedNode>
 
   beforeEach(() => {
     node = cf({ dataset: $rdf.dataset() })
-      .namedNode('http://example.com/Proxied') as any
+      .namedNode(ex.Proxied) as any
 
-    node.addOut(node.namedNode('http://example.com/Prop1'), node.namedNode('http://example.com/Proxied2'), p => {
-      p.addOut(node.namedNode('http://example.com/Prop2'), node.literal('foo', 'bar'))
+    node.addOut(ex.Prop1, ex.Proxied2, p => {
+      p.addOut(ex.Prop2, node.literal('foo', 'bar'))
     })
+
+    node.addOut(ex.multi, ['a', 'z'])
   })
 
   it('proxies direct index access', () => {
@@ -23,7 +28,7 @@ describe('proxy', () => {
     const proxy = createProxy(resource)
 
     // when
-    const value = proxy['http://example.com/Prop1'][0]
+    const value = proxy['http://example.com/Prop1']
 
     // then
     expect(value.id.value).toEqual('http://example.com/Proxied2')
@@ -35,10 +40,22 @@ describe('proxy', () => {
     const proxy = createProxy(resource)
 
     // when
-    const value = proxy['http://example.com/Prop1'][0]['http://example.com/Prop2'][0]
+    const value = proxy['http://example.com/Prop1']['http://example.com/Prop2']
 
     // then
     expect(value).toEqual(node.literal('foo', 'bar').term)
+  })
+
+  it('returns array from multiple values', () => {
+    // given
+    const resource = new RdfResourceImpl(node)
+    const proxy = createProxy(resource)
+
+    // when
+    const value = proxy['http://example.com/multi'] as Literal[]
+
+    // then
+    expect(value.map(l => l.value)).toStrictEqual(['a', 'z'])
   })
 
   it('does not proxy built-in properties', () => {
