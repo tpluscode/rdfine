5c98a213dc0d2d0bc78a6ffd291dd5677ec7f1e3
tpluscode <tpluscode@users.noreply.github.com>
2020-01-16 10:14:49 +0100

    fix: relax the typings of create method

--- src/__tests__/RdfResource.spec.ts
+++ src/__tests__/RdfResource.spec.ts
@@ -3,6 +3,27 @@ import $rdf from 'rdf-ext'
 import RdfResource from '../lib/RdfResource'
 
 describe('RdfResource', () => {
+  describe('constructor', () => {
+    it('throws when node is literal', () => {
+      expect(() => {
+        const node = cf({ dataset: $rdf.dataset() }).literal('foo')
+
+        return new RdfResource(node)
+      }).toThrow()
+    })
+
+    it('throws when context represents multiple nodes', () => {
+      expect(() => {
+        const node = cf({
+          dataset: $rdf.dataset(),
+          term: [$rdf.blankNode(), $rdf.blankNode()],
+        })
+
+        return new RdfResource(node)
+      }).toThrow()
+    })
+  })
+
   describe('equals', () => {
     it('compares id term', () => {
       // given
