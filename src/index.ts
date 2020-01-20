dba91775d3473bbfc7aced90cc504e037bae4ea4
tpluscode <tpluscode@users.noreply.github.com>
2020-01-20 10:00:18 +0100

    feat: export indexer interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { namespace } from './lib/decorators/namespace'
-export { ResourceFactory, Constructor } from './lib/ResourceFactory'
+export { ResourceFactory, Constructor, ResourceIndexer } from './lib/ResourceFactory'
 export { default as RdfResourceImpl, RdfResource, ResourceIdentifier } from './lib/RdfResource'
 export { property } from './lib/decorators/property'
