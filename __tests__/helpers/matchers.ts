e0a59d99b11d5dcd8afdb484868ffa5e6ceb9330
tpluscode <tpluscode@users.noreply.github.com>
2020-07-07 11:41:02 +0200

    ci: move jest matchers to one file

--- __tests__/helpers/matchers.ts
+++ __tests__/helpers/matchers.ts
@@ -1,12 +1,23 @@
 import { toMatchSnapshot } from 'jest-snapshot'
 import DatasetExt from 'rdf-ext/lib/Dataset'
+import { SourceFile } from 'ts-morph'
 
 expect.extend({
-  toMatchSnapshot(received: DatasetExt | object) {
+  toMatchSnapshot(received: DatasetExt | SourceFile | object) {
     if (typeof received === 'object' && received && 'toCanonical' in received) {
       return toMatchSnapshot.call(
         this as any,
         received.toCanonical(),
+        'toMatchSnapshot',
+      )
+    }
+
+    if (received instanceof SourceFile) {
+      received.saveSync()
+      const contents = received.getProject().getFileSystem().readFileSync(received.getFilePath())
+      return toMatchSnapshot.call(
+        this as any,
+        contents,
         'toMatchSnapshot',
       )
     }
