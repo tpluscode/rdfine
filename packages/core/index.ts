a9e85e30b81c0c4ea2d1e7218cd52c3e01c66456
tpluscode <tpluscode@users.noreply.github.com>
2020-09-02 12:36:47 +0200

    feat(core): export ResourceFactory

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,7 +1,8 @@
 import RdfResourceImpl from './RdfResource'
 
 export { crossBoundaries } from './lib/path'
-export type { ResourceFactory, Constructor, ResourceIndexer } from './lib/ResourceFactory'
+export type { Constructor, ResourceIndexer } from './lib/ResourceFactory'
+export { default as ResourceFactory } from './lib/ResourceFactory'
 export type { RdfResource, ResourceIdentifier } from './RdfResource'
 export * from './lib/decorators'
 export default RdfResourceImpl
