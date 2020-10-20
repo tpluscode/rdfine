d13a929ddb03219c573f1b6b3d42a6623ad9251e
tpluscode <tpluscode@users.noreply.github.com>
2020-10-20 16:49:20 +0200

    fix: collection did not work with augmented base interface

---
+++ .changeset/young-snakes-joke.md
@@ -0,0 +1,5 @@
+---
+"@rdfine/hydra": patch
+---
+
+Relax hydra:Collection generic argument
--- vocabularies/hydra/Collection.ts
+++ vocabularies/hydra/Collection.ts
@@ -2,12 +2,12 @@ import RdfResourceImpl, { Constructor, namespace, RdfResource, property } from '
 import * as $rdf from '@rdf-esm/data-model';
 import type * as RDF from 'rdf-js';
 import { hydra } from './lib/namespace';
-import type { Initializer, ResourceNode } from '@tpluscode/rdfine/RdfResource';
+import type { Initializer, RdfResourceCore, ResourceNode } from '@tpluscode/rdfine/RdfResource';
 import type { Mixin } from '@tpluscode/rdfine/lib/ResourceFactory';
 import type * as Hydra from '.';
 import { ResourceMixin } from './Resource';
 
-export interface Collection<M extends RdfResource<any> = RdfResource<any>, D extends RDF.DatasetCore = RDF.DatasetCore> extends Hydra.Resource<D>, RdfResource<D> {
+export interface Collection<M extends RdfResourceCore<any> = RdfResourceCore<any>, D extends RDF.DatasetCore = RDF.DatasetCore> extends Hydra.Resource<D>, RdfResource<D> {
   manages: Array<Hydra.ManagesBlock<D>>;
   member: Array<Hydra.Resource<D> & M>;
   totalItems: number | undefined;
