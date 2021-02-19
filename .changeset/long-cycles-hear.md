5c1d0a8a34cc5f937b3a68f8c0cdf42bd7b81f62
tpluscode <tpluscode@users.noreply.github.com>
2021-02-19 22:08:19 +0100

    fix: initialize single element without RDF list

---
+++ .changeset/long-cycles-hear.md
@@ -0,0 +1,5 @@
+---
+"@tpluscode/rdfine": patch
+---
+
+Property which allows lists and single element should not initalize RDF List with single item
--- vocabularies/shacl/__tests__/PropertyShape.spec.ts
+++ vocabularies/shacl/__tests__/PropertyShape.spec.ts
@@ -18,7 +18,6 @@ describe('PropertyShape', () => {
       const graph = clownface({ dataset, term: RDF.namedNode('http://example.com/shape') })
 
       // when
-      // eslint-disable-next-line no-new
       fromPointer(graph, {
         path: rdfs.label,
       })
@@ -33,13 +32,40 @@ describe('PropertyShape', () => {
       const graph = clownface({ dataset, term: RDF.namedNode('http://example.com/shape') })
 
       // when
-      // eslint-disable-next-line no-new
       fromPointer(graph, {
         path: [skos.broader, rdfs.label],
       })
 
       // then
       expect(dataset.toCanonical()).toMatchSnapshot()
+    })
+
+    it('initializes a single path property without an RDF List', () => {
+      // given
+      const dataset = $rdf.dataset()
+      const graph = clownface({ dataset, term: RDF.namedNode('http://example.com/shape') })
+
+      // when
+      const shape = fromPointer(graph, {
+        path: rdfs.label,
+      })
+
+      // then
+      expect(shape).toMatchSnapshot()
+    })
+
+    it('initializes a single element array as an RDF List', () => {
+      // given
+      const dataset = $rdf.dataset()
+      const graph = clownface({ dataset, term: RDF.namedNode('http://example.com/shape') })
+
+      // when
+      const shape = fromPointer(graph, {
+        path: [rdfs.label],
+      })
+
+      // then
+      expect(shape).toMatchSnapshot()
     })
   })
 })
