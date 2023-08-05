91486836b2a3c5bf0a93e8454ea98f68bedf2267
tpluscode <tpluscode@users.noreply.github.com>
2023-08-05 10:32:30 +0200

    refactor: use @zazuko/env

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,26 +1,8 @@
-import Environment from '@rdfjs/environment/Environment.js'
 import { RdfineFactory } from '@tpluscode/rdfine'
-import DataFactory from '@rdfjs/data-model/Factory.js'
-import DatasetFactory from '@rdfjs/dataset/Factory.js'
-import NamespaceFactory from '@rdfjs/namespace/Factory.js'
-import ClownfaceFactory from 'clownface/Factory.js'
-import TermSetFactory from '@rdfjs/term-set/Factory.js'
-import TermMapFactory from '@rdfjs/term-map/Factory.js'
+import type { FactoryConstructor } from '@rdfjs/environment/Environment'
+import { create } from '@zazuko/env'
 import { RdfineEnvironment } from '@tpluscode/rdfine/environment'
 
-interface FactoryConstructor<F = object> {
-  new (...args: any[]): F
-}
-
 export function createEnv<T extends FactoryConstructor>(...factories: T[]): RdfineEnvironment {
-  return <any> new Environment([
-    RdfineFactory,
-    DataFactory,
-    DatasetFactory,
-    NamespaceFactory,
-    ClownfaceFactory,
-    TermSetFactory,
-    TermMapFactory,
-    ...factories,
-  ])
+  return <any> create(RdfineFactory as any, ...factories)
 }
